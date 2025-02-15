3521e8b929404d7f0d590e87161f492fc927f3c6
yuanrh2004 <yuanrh2004@gmail.com>
2025-02-15 11:27:32 +0800

    theme-updated

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "furo"
+html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
 
