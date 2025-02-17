b3a7c8323b9cf1dc9123a6c933da216d616a3e07
yuanrh <yuanrh2004@gmail.com>
2025-02-17 21:48:33 +0800

    sVDJ_updated0217

--- src/B_HIT/sVDJ/tl/__init__.py
+++ src/B_HIT/sVDJ/tl/__init__.py
@@ -1,4 +1,4 @@
 from .compute_grouped_index import compute_grouped_index
 from .spatial_bcr_desc import compute_index
 from .summarize_BCR import compute_clone_counts, compute_richness
-from .compute_correlation import compute_correlation
+from .compute_correlation import compute_correlation, compute_groupwise_corr_matrix
--- src/B_HIT/sVDJ/tl/compute_correlation.py
+++ src/B_HIT/sVDJ/tl/compute_correlation.py
@@ -47,3 +47,48 @@ def compute_correlation(cloneRich, groupby_cols, corr1, corr2, save=False, path=
     # Return the DataFrame containing correlation results
     return corrDf
 
+# grouped_correlations = {}
+# for group, group_data in tmp_df.groupby(['Cregion_simple','tissue']):
+#     group_data = group_data.dropna(subset=['Clonal_diversification'])
+#     correlation, _ = pearsonr(group_data['BaggArea'], group_data['Clonal_diversification'])
+#     grouped_correlations[group] = (correlation, _)
+# print(grouped_correlations)
+
+def compute_groupwise_corr_matrix(grouped_correlations, corr_col, pvalue_col, groupby_cols, save=False, path=None):
+    """
+    Compute the Pearson correlation and p-value for each group defined by groupby_cols and return the correlation and p-value matrices.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The input DataFrame containing the data.
+    corr_col : str
+        The column name for which the Pearson correlation is computed (e.g., 'BaggArea').
+    pvalue_col : str
+        The column name for which the p-value is calculated (e.g., 'cloneFamilyRichness').
+    groupby_cols : list of str
+        List of columns to group by (e.g., ['chain', 'region']).
+    save : bool, optional
+        Whether to save the resulting correlation and p-value matrices as CSV files. Default is False.
+    path : str, optional
+        The file path to save the matrices if save=True.
+
+    Returns
+    -------
+    tuple of pd.DataFrame
+        Two DataFrames: one for the correlation matrix and one for the p-value matrix.
+    """
+
+    corrDf = pd.DataFrame(grouped_correlations).T
+    corrDf.columns = ['corr', 'Pvalue']
+    
+    corrmat = corrDf[['corr']].copy().pivot_table(index=groupby_cols[0], columns=groupby_cols[1])
+    corrmat.columns = corrmat.columns.get_level_values(1)  # Flatten the multi-index of columns
+    Pmat = corrDf[['Pvalue']].copy().pivot_table(index=groupby_cols[0], columns=groupby_cols[1])
+    Pmat.columns = Pmat.columns.get_level_values(1)  # Flatten the multi-index of columns
+    
+    if save and path:
+        corrmat.to_csv(f'{path}_correlation_matrix.csv')
+        Pmat.to_csv(f'{path}_pvalue_matrix.csv')
+
+    return corrmat, Pmat
